f3cceb3db84469cbf747d9cde2f445b6b183cd1b
Yash Vekariya <suratgmryash@gmail.com>
2026-06-06 15:44:26 +0530

    Implement leftRightDifference function in Solution class

---
+++ Problems/(Easy)2574LeftandRightSumDifferences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> answer(n);
+        
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+        
+        int leftSum = 0;
+        int rightSum = totalSum;
+
+        for (int i = 0; i < n; i++) {
+            rightSum -= nums[i];             
+            answer[i] = abs(leftSum - rightSum);
+            leftSum += nums[i]; 
+        }
+        
+        return answer;
+    }
+};
