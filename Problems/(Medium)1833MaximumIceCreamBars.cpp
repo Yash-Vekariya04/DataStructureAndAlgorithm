c13c05690d2285c676c17ea189de673065f8d3c4
Yash Vekariya <suratgmryash@gmail.com>
2026-06-22 01:54:17 +0530

    Implement maxIceCream function to calculate maximum bars

---
+++ Problems/(Medium)1833MaximumIceCreamBars.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(), costs.end());
+        int count = 0;
+        for(int c : costs){
+            if(coins >= c){
+                count++;
+                coins -= c;
+            } else{
+                break;
+            }
+        }
+
+        return count;
+    }
+};
